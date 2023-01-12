061229fd3f37da6c2e721e6c317b09c91d88767a
Pedro <peedrofranq96@gmail.com>
2023-01-12 01:28:14 -0300

    Executando as migrations e criando as tabelas no banco.

--- GeekShopping.ProductAPI/Model/Context/SqlServerContext.cs
+++ GeekShopping.ProductAPI/Model/Context/SqlServerContext.cs
@@ -6,5 +6,15 @@ namespace GeekShopping.ProductAPI.Model.Context
 	{
 		public SqlServerContext() {}
 		public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options) { }
+
+		public DbSet<Product> Products { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.Entity<Product>()
+			   .Property(p => p.Price).HasColumnType("decimal(38,02)").IsRequired();
+		}
 	}
 }
