eb581d8afe302898c1161b9246689576f2566de2
Pedro <peedrofranq96@gmail.com>
2023-01-13 19:09:57 -0300

    Configurando o Cart Controller do CartAPI

--- GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -30,7 +30,7 @@ namespace GeekShopping.ProductAPI.Controllers
 
 		[HttpGet("{id}")]
 		[Authorize]
-		public async Task<ActionResult> FindById(long id)
+		public async Task<ActionResult<ProductVO>> FindById(long id)
 		{
 			var product = await _repository.FindById(id);
 			if (product == null) return NotFound();
