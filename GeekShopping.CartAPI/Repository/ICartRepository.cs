2bc3928963fe6d3abb3bbc53a79236547abac79c
Pedro <peedrofranq96@gmail.com>
2023-01-13 18:52:47 -0300

    Finalizando o repository do CartAPI

--- GeekShopping.CartAPI/Repository/ICartRepository.cs
+++ GeekShopping.CartAPI/Repository/ICartRepository.cs
@@ -5,7 +5,7 @@ namespace GeekShopping.CartAPI.Repository
 {
 	public interface ICartRepository
 	{
-		Task<CartVO> FindCarByUserId(string userId);
+		Task<CartVO> FindCartByUserId(string userId);
 		Task<CartVO> SaveOrUpdateCart(CartVO cart);
 		Task<bool> RemoveFromCart(long  cartDetailsId);
 		Task<bool> ApplyCoupon(string userId, string couponCode);
