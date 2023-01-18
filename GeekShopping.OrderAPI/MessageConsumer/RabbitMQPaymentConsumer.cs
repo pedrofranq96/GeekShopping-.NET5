aad450eb6a367464be7f1047a353298bddb1268f
Pedro <peedrofranq96@gmail.com>
2023-01-18 19:07:35 -0300

    Refatorando a queue para Exchange

--- GeekShopping.OrderAPI/MessageConsumer/RabbitMQPaymentConsumer.cs
+++ GeekShopping.OrderAPI/MessageConsumer/RabbitMQPaymentConsumer.cs
@@ -19,10 +19,12 @@ namespace GeekShopping.OrderAPI.MessageConsumer
 		private readonly OrderRepository _repository;
 		private IConnection _connection;
 		private IModel _channel;
+		private const string ExchangeName = "FanoutPaymentUpdate";
+		string queueName = "";
 
 		public RabbitMQPaymentConsumer(OrderRepository repository)
 		{
-			_repository = repository;			
+			_repository = repository;
 			var factory = new ConnectionFactory
 			{
 				HostName = "localhost",
@@ -31,7 +33,9 @@ namespace GeekShopping.OrderAPI.MessageConsumer
 			};
 			_connection = factory.CreateConnection();
 			_channel = _connection.CreateModel();
-			_channel.QueueDeclare(queue: "orderpaymentresultqueue", false, false, false, arguments: null);
+			_channel.ExchangeDeclare(ExchangeName, ExchangeType.Fanout);
+			queueName = _channel.QueueDeclare().QueueName;
+			_channel.QueueBind(queueName, ExchangeName, "");
 		}
 
 		protected override Task ExecuteAsync(CancellationToken stoppingToken)
@@ -45,7 +49,7 @@ namespace GeekShopping.OrderAPI.MessageConsumer
 				UpdatePaymentStatus(vo).GetAwaiter().GetResult();
 				_channel.BasicAck(evt.DeliveryTag, false);
 			};
-			_channel.BasicConsume("orderpaymentresultqueue", false, consumer);
+			_channel.BasicConsume(queueName, false, consumer);
 			return Task.CompletedTask;
 		}
 
