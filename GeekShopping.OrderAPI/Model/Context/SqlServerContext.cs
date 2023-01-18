0e7cf51e7b15e8f7863fdc2a567bec7c8f2177fb
Pedro <peedrofranq96@gmail.com>
2023-01-18 12:11:30 -0300

    Adicionando ao projeto o MS OrderAPI

---
+++ GeekShopping.OrderAPI/Model/Context/SqlServerContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.OrderAPI.Model.Base
+{
+	public class SqlServerContext : DbContext
+	{
+
+		public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options) { }
+
+		public DbSet<OrderDetail> Details { get; set; }
+		public DbSet<OrderHeader> Headers { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.Entity<OrderDetail>()
+			   .Property(p => p.Price).HasColumnType("decimal(38,2)").IsRequired();
+
+			builder.Entity<OrderHeader>()
+			   .Property(d => d.DiscountTotal).HasColumnType("decimal(38,2)").IsRequired();
+
+			builder.Entity<OrderHeader>()
+			   .Property(p => p.PurchaseAmount).HasColumnType("decimal(38,2)").IsRequired();
+		}
+	}
+}
---
+++ GeekShopping.OrderAPI/Model/OrderHeader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.OrderAPI.Model.Base
+{
+	[Table("order_header")]
+	public class OrderHeader : BaseEntity
+	{
+		[Column("user_id")]
+		public string UserId { get; set; }
+
+		[Column("coupon_code")]
+		public string CouponCode { get; set; }
+		[Column("purchase_amount")]
+		public decimal PurchaseAmount { get; set; }
+		[Column("discount_total")]
+		public decimal DiscountTotal { get; set; }
+		[Column("firstName")]
+		public string FirstName { get; set; }
+		[Column("lastName")]
+		public string LastName { get; set; }
+		[Column("purchase_date")]
+		public DateTime Datatime { get; set; }
+		[Column("order_time")]
+		public DateTime OrderTime { get; set; }
+		[Column("phone_number")]
+		public string Phone { get; set; }
+		[Column("email")]
+		public string Email { get; set; }
+		[Column("cardNumber")]
+		public string CardNumber { get; set; }
+		[Column("cvv")]
+		public string CVV { get; set; }
+		[Column("expire_month_year")]
+		public string ExpireMonthYear { get; set; }
+		[Column("total_itens")]
+		public int CartTotalItens { get; set; }
+		public IEnumerable<OrderDetail> OrderDetails { get; set; }
+		[Column("payment_status")]
+		public bool PaymentStatus { get; set; }
+	}
+}
