a896c577572495a69cc3843040fe04737610b1b4
Pedro <peedrofranq96@gmail.com>
2023-01-19 13:03:28 -0300

    Configurando o ocelot no projeto

--- GeekShopping.GatewayAPI/Startup.cs
+++ GeekShopping.GatewayAPI/Startup.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace GeekShopping.GatewayAPI
 {
@@ -27,6 +23,17 @@ namespace GeekShopping.GatewayAPI
 		{
 
 			services.AddControllers();
+
+			services.AddAuthentication("Bearer")
+				.AddJwtBearer("Bearer", options =>
+				{
+					options.Authority = "https://localhost:4435/";
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateAudience = false
+					};
+				});
+			services.AddOcelot();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,16 +44,7 @@ namespace GeekShopping.GatewayAPI
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.UseHttpsRedirection();
-
-			app.UseRouting();
-
-			app.UseAuthorization();
-
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
-			});
+			app.UseOcelot();
 		}
 	}
 }
